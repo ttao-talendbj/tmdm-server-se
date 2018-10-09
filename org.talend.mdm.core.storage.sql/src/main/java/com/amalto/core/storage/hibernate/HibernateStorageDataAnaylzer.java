52597e136065b130533718bac712d17fe617014c
zfkang-talend <zfkang@talend.com>
2018-10-09 18:10:45 +0800

    TMDM-11044 [Impact Analysis] Adapt Low/Medium Impact to Foreign Key field

--- org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/hibernate/HibernateStorageDataAnaylzer.java
+++ org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/hibernate/HibernateStorageDataAnaylzer.java
@@ -12,16 +12,20 @@ package com.amalto.core.storage.hibernate;
 
 import static com.amalto.core.query.user.UserQueryBuilder.count;
 import static com.amalto.core.query.user.UserQueryBuilder.emptyOrNull;
+import static com.amalto.core.query.user.UserQueryBuilder.not;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.talend.mdm.commmon.metadata.*;
+import org.talend.mdm.commmon.metadata.compare.AddChange;
 import org.talend.mdm.commmon.metadata.compare.Change;
 import org.talend.mdm.commmon.metadata.compare.Compare;
 import org.talend.mdm.commmon.metadata.compare.HibernateStorageImpactAnalyzer;
 import org.talend.mdm.commmon.metadata.compare.ModifyChange;
+import org.talend.mdm.commmon.metadata.compare.RemoveChange;
 
 import com.amalto.core.query.user.UserQueryBuilder;
 import com.amalto.core.storage.StorageResults;
@@ -64,6 +68,46 @@ public class HibernateStorageDataAnaylzer extends HibernateStorageImpactAnalyzer
                     } finally {
                         storage.commit();
                     }
+                }
+            }
+        }
+
+        Map<FieldMetadata, RemoveChange> removeReference = new HashMap<>();
+        Map<RemoveChange, ComplexTypeMetadata> renamedReference = new HashMap<>();
+        for (RemoveChange removeAction : diffResult.getRemoveChanges()) {
+            MetadataVisitable element = removeAction.getElement();
+            if (element instanceof ReferenceFieldMetadata) {
+                removeReference.put(((ReferenceFieldMetadata) element).getReferencedField(), removeAction);
+            }
+        }
+        for (AddChange removeAction : diffResult.getAddChanges()) {
+            MetadataVisitable element = removeAction.getElement();
+            if (element instanceof ReferenceFieldMetadata) {
+                if (removeReference.containsKey(((ReferenceFieldMetadata) element).getReferencedField())) {
+                    renamedReference.put((removeReference.get(((ReferenceFieldMetadata) element).getReferencedField())),
+                            ((ReferenceFieldMetadata) element).getContainingType().getEntity());
+                }
+            }
+        }
+        if (!renamedReference.isEmpty()) {
+            for (Map.Entry<RemoveChange, ComplexTypeMetadata> entry : renamedReference.entrySet()) {
+                try {
+                    storage.begin();
+                    ComplexTypeMetadata objectType = entry.getValue();
+                    UserQueryBuilder qb = UserQueryBuilder.from(objectType).select(count())
+                            .where(not(emptyOrNull((FieldMetadata) entry.getKey().getElement())));
+                    StorageResults results = storage.fetch(qb.getSelect());
+                    if (results.getCount() == 0) {
+                        entry.getKey().setContainsData(false);
+                    } else {
+                        entry.getKey().setContainsData(true);
+                    }
+                    results.close();
+                } catch (Exception e) {
+                    LOGGER.error("Hibernate Storage query data anaylzer failure", e);
+                    storage.rollback();
+                } finally {
+                    storage.commit();
                 }
             }
         }
